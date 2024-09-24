e623327e01e00ce38b7ef05aa70746fb0c2c0902
alexpkgs <alexpkgs@noreply.localhost>
2024-09-23 22:40:02 -0400

    Starting my Emacs Config not done...

---
+++ cfg/emacs/init.el
@@ -0,0 +1,11 @@
+(add-to-list 'default-frame-alist '(font . "Cartograph CF 10"))
+(set-face-attribute 'default t :font "Cartograph CF 10")
+(set-face-attribute 'default nil :font "Cartograph CF 10")
+(set-frame-font "Cartograph CF 10" nil t)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
+(use-package evil :ensure t :init
+  (evil-mode +1))
