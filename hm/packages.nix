6815f74255d6e912027b86b715fabc174e847f68
alexpkgs <alexpkgs@noreply.localhost>
2024-09-28 00:48:47 -0400

    removing pkgs i don't really need anymore

--- hm/packages.nix
+++ hm/packages.nix
@@ -2,12 +2,9 @@
 
 {
   home.packages = [
-    pkgs.lua
-    pkgs.dunst
     pkgs.rofi
     pkgs.starship
     pkgs.nerd-font-patcher
-    pkgs.xwallpaper
     pkgs.vim
     pkgs.fff
     pkgs.emacs
